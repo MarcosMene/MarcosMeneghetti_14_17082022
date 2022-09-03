16685d293bd7598890f99734e19a7748714be50e
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-09-03 10:18:14 +0200

    remove navigate to netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import CreateEmployee from "./Pages/createEmployee/CreateEmployee";
 import EmployeList from "./Pages/employList/EmployeeList";
