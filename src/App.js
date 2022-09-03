3fe149a3a24a6c06caa0ea51e333760e328344f5
Marcos Meneghetti <73244966+MarcosMene@users.noreply.github.com>
2022-09-03 15:51:39 +0200

    hashrouter deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import CreateEmployee from "./Pages/createEmployee/CreateEmployee";
 import EmployeList from "./Pages/employList/EmployeeList";
@@ -7,8 +7,8 @@ import "./App.css";
 
 const App = () => {
   return (
-    // <BrowserRouter basename="/MarcosMeneghetti_14_17082022">
-    <BrowserRouter basename="/MarcosMeneghetti_14_17082022">
+    // <BrowserRouter basename="/MarcosMeneghetti_14_17082022"></BrowserRouter>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<CreateEmployee />} />
